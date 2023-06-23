cb75f888d25b57d21180338f71f64d02d7c9ac62
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2023-06-24 00:28:28 +0200

    Update step0_download_data.sh

--- step0_download_data.sh
+++ step0_download_data.sh
@@ -20,81 +20,15 @@ export output=$(echo "text")  # json, yaml, yaml-stream, text, table
 # ---------------------------------------------
 # Setup ROOT AWS credentials 
 # ---------------------------------------------
-export val=$(echo "X0")
 
-if [[ $val == "X0" ]]
-then 
-    # Generate a new ROOT access key : This information gets sent to the /home/oem2/.aws/credentials and /home/oem2/.aws/config files
-    # AWS site: Go to https://aws.amazon.com/fr/ 
-    # 	- Services - Security, Identity, & Compliance - IAM
-    # 	- To go to root user settings : Quick Links - My security credentials
-    # 	- Open CloudShell - aws iam create-access-key
-    # 
-    #}   "CreateDate": "2023-06-21T09:20:32+00:00"ZY2s6SVxna6uvGfCCDK",
-    #{
-    #"AccessKey": {
-    #    "AccessKeyId": "AKIAUZDPRVKIJFUOJANI",
-    #    "Status": "Active",
-    #    "SecretAccessKey": "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",
-    #    "CreateDate": "2023-06-21T09:20:32+00:00"
-    #}
-    #
-    # OR
-    # 
-    # PC terminal (did not work) - aws iam create-access-key
-    
-    # Set configuration file : aws_access_key_id and aws_secret_access_key are automatically put in /home/oem2/.aws/credentials and /home/oem2/.aws/config files
-    aws configure set region $region
-    aws configure set output $output
-    
-    export AWS_ACCESS_KEY_ID=$(echo "AKIAUZDPRVKIJFUOJANI")
-    export AWS_SECRET_ACCESS_KEY=$(echo "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")
-else
-    echo "Do not setup ROOT AWS credentials"
-fi
 
 
 
 # ---------------------------------------------
 # Setup USER AWS credentials 
 # ---------------------------------------------
-export val=$(echo "X1")
-
-if [[ $val == "X0" ]]
-then 
-    # Follow instructions at CREATE a username
-    
-    export USERNAME=$(echo "jamilah")
-    
-    aws iam create-access-key --user-name $USERNAME
-    
-    # Set configuration file: put information into /home/oem2/.aws/credentials and /home/oem2/.aws/config files
-    aws configure set aws_access_key_id AKIAUZDPRVKIPHOVBEXG --profile $USERNAME
-    aws configure set aws_secret_access_key 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 --profile $USERNAME
-    aws configure set region $region --profile $USERNAME
-    aws configure set output $output --profile $USERNAME
-    
-    # Set environmental variables
-    export AWS_ACCESS_KEY_ID=$(echo "AKIAUZDPRVKIPHOVBEXG")
-    export AWS_SECRET_ACCESS_KEY=$(echo "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")
-else
-    echo "Do not setup USER AWS credentials"
-fi
 
 
-
-
-# ---------------------------------------------
-# Make random number for creating variable or names
-# ---------------------------------------------
-if [[ $val == "X1" ]]
-then 
-    let "randomIdentifier=$RANDOM*$RANDOM"
-else
-    let "randomIdentifier=202868496"
-fi
-
-# ---------------------------------------------
 
 
 
